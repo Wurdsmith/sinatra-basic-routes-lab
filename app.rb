53777e285881d7f61e4a4422c1130cb8cccc1a9d
Wurdsmith <wurdsmithing@gmail.com>
2021-02-22 23:38:45 -0600

    commit tree d60bd891b2ac60c7eae4ee7ffa5c6404b6b9061a

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+      end
+    
+      get '/name' do
+        "My name is Andrew"
+      end
+    
+      get '/hometown' do
+        "My hometown is the Twin Cities"
+      end
+    
+      get '/favorite-song' do
+        "My favorite song is The Battle of Evermore"
+      end
 end
